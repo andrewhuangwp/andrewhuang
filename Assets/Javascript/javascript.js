c11af8f12e32e0e0ae4578dafd5be512438347a3
andrewhuangwp <andrewhuangwp@gmail.com>
2020-01-05 17:13:03 -0500

    Update javascript.js

--- Assets/Javascript/javascript.js
+++ Assets/Javascript/javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(function(){
         var pathname = (window.location.pathname.match(/[^\/]+$/)[0]);
 
-        $('sidenav a').each(function() {
+        $('.sidenav a').each(function() {
         if ($(this).attr('href') == pathname)
         {
             $(this).addClass('current');
