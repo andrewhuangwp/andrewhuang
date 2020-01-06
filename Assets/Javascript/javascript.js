2c1d3c6fdf20cfe205e688474950916857bed7e9
andrewhuangwp <andrewhuangwp@gmail.com>
2020-01-05 20:48:34 -0500

    Updating Index

--- Assets/Javascript/javascript.js
+++ Assets/Javascript/javascript.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    /* Highlight Current Page*/
     $(function(){
         var pathname = (window.location.pathname.match(/[^\/]+$/)[0]);
 
@@ -8,5 +9,23 @@ $(document).ready(function(){
             $(this).addClass('current');
         }
         });
+    });
+    /* Resize Texts based on Window size*/
+    $(window).resize(function() {
+        var h = $(window).height();
+        var w = $(window).width();
+         
+        /*Update sidebar to be horizontal if resized to a certain size*/
+        if((h/w)>0.61) {
+            $('.sidenav h3').css("font-size", "18px");
+            $('.sidenav p').css("font-size", "12px");
+            $('.sidenav a').css("font-size", "14px");
+        }
+        /*Update sidebar to be vertical if resized to a certain size*/
+        if((h/w)<0.61) {
+            $('.sidenav h3').css("font-size", "32px");
+            $('.sidenav p').css("font-size", "16px");
+            $('.sidenav a').css("font-size", "25px");
+        }
     });
 });
