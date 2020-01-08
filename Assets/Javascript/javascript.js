3cdde90748e30667949db8f2bbb7168d6c5753fe
andrewhuangwp <andrewhuangwp@gmail.com>
2020-01-07 22:45:00 -0500

    Fix Default webpage error

--- Assets/Javascript/javascript.js
+++ Assets/Javascript/javascript.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         var pathname = (window.location.pathname.match(/[^\/]+$/)[0]);
 
         $('nav a').each(function () {
-            if ($(this).attr('href') == pathname) {
+            if ($(this).attr('href') == pathname || '') {
                 $(this).addClass('current');
             }
         });
